4c3c5b98203f18f662b08196e1602c7644ac1608
gonta1026 <katano.original@gmail.com>
2020-06-18 09:46:57 +0900

    sprintf

--- basics/sprintf.php
+++ basics/sprintf.php
@@ -1,16 +1,24 @@
 <?php
-// 5W
-$what = "たべた！";
-$why = "たべた！";
+
 $num = 5;
 $location = 'tree';
+
 
 // $format = 'The %s contains %d monkeys';
 $format = 'The %2$s contains %1$d monkeys.
            That\'s a nice %2$s full of %1$d monkeys. %3$s';
-echo sprintf($format, $num, $location, "こんにちわ");
+echo sprintf($format, $num, $location);
 
 // %のあとの数字がindexになる。$04のところでけたの数字で指定している。
 $format = 'The %2$s contains %1$04d monkeys';
 echo sprintf($format, $num, $location);
 //output The tree contains 0005 monkeys
+
+// ----------------------------try-------------------------------
+
+$what = "肉";
+$how = 5;
+$when = "今日";
+$do = "たべた！";
+$format = "%sを%dつ%s%s";
+echo sprintf($format, $what, $how, $when, $do);
